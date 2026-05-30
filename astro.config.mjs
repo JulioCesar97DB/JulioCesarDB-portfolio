1005d43b69e4188c86c9ee58a40c2d940b8bbf0e
Julio Cesar Diaz <juliocesar97db@gmail.com>
2026-05-30 11:39:26 -0400

    feat: update configuration and type definitions for improved module resolution

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@cv": fileURLToPath(new URL("./cv.json", import.meta.url)),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
 });
